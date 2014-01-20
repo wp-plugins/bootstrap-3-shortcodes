3168bb935ecdfbaed447fe3b957fec004c9b866c
FoolsRun <FoolsRun@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-20 03:25:34 +0000

    3.0.3.1 release
    git-svn-id: https://plugins.svn.wordpress.org/bootstrap-3-shortcodes/tags/3.0.3.1@841531 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/bootstrap-shortcodes-help.php
@@ -0,0 +1,57 @@
+<?php
+
+// We need a function that can add ids to HTML header tags
+function retitle($match) {
+    list($_unused, $h3, $title) = $match;
+
+    $id = strtolower(strtr($title, " .", "--"));
+
+    return "<$h3 id='$id'>$title</$h3>";
+}
+
+$thisfile = realpath(dirname(__FILE__));
+# Install PSR-0-compatible class autoloader
+spl_autoload_register(function($class){
+    require 'php_markdown/' . preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+});
+
+# Get Markdown class
+//use \Michelf\Markdown;
+use \Michelf\MarkdownExtra;
+
+$text = file_get_contents(dirname(__FILE__) . '/../README.md');
+$html = MarkdownExtra::defaultTransform($text);
+?>
+
+<script>
+    jQuery(document).ready(function() {
+        jQuery(".insert-code").click(function() {
+            var example = jQuery( this ).parent().prev().find("code").text();
+            var lines = example.split('\n');
+            var paras = '';
+            jQuery.each(lines, function(i, line) {
+                if (line) {
+                    paras += line + '<br>';
+                }
+            });
+            var win = window.dialogArguments || opener || parent || top;
+            win.send_to_editor(paras);
+        });
+    });
+</script>
+        <div style="display:none;" id="bootstrap-shortcodes-help-popup">
+            <div id="bootstrap-shortcodes-help">
+            <?php
+                # Put HTML content in the document
+                $html = preg_replace('/(<a href="http:[^"]+")>/is','\\1 target="_blank">',$html);
+                $html = str_replace('<table>', '<table class="table table-striped">', $html);
+                $html = str_replace('<ul>', '<div class="list-group">', $html);
+                $html = str_replace('</ul>', '</div">', $html);
+                $html = str_replace('<li><a ', '<a class="list-group-item" ', $html);
+                $html = str_replace('</li>', '', $html);
+                $html = preg_replace_callback("#<(h[1-6])>(.*?)</\\1>#", "retitle", $html);
+                $html = str_replace('</pre>', '</pre><p><button class="btn btn-primary btn-sm insert-code">Insert Example <i class="glyphicon glyphicon-share-alt"></i></button></p>', $html);
+                echo $html;
+            ?>
+            </div>
+        </div>
---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== Bootstrap Shortcodes for WordPress ===
+Contributors: filipstefansson, nodley, FoolsRun
+Tags: bootstrap, shortcode, shortcodes, responsive, grid
+Requires at least: 3.8
+Tested up to: 3.8
+Stable tag: 3.0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Implements Bootstrap 3 styles and components in WordPress through shortcodes.
+
+== Description ==
+
+###Just The Shortcodes, Please
+Plenty of great WordPress plugins focus on providing or including the Bootstrap library into your site. **Bootstrap Shortcodes for WordPress** assumes you're working with a theme that already includes Bootstrap 3 and focuses on giving you a great set of shortcodes to use it with.
+
+This plugin creates a simple, out of the way button just above the WordPress TinyMCE editor (next to the "Add Media" button) which pops up the plugin's documentation and shortcode examples for reference and handy "Insert Example" links to send the example shortcodes straight to the editor. There are no additional TinyMCE buttons to clutter up your screen, just great, easy to use shortcodes!
+
+If you like this plugin, check out our companion plugin for Font Awesome, [Font Awesome Shortcodes](http://www.wordpress.org/plugins/font-awesome-shortcodes/)
+
+###Supported Shortcodes
+####CSS
+* Grid (fully responsive)
+* Lead body copy
+* Emphasis classes
+* Code
+* Tables
+* Buttons
+* Responsive utilities
+####Components
+* Icons
+* Button Groups
+* Labels
+* Badges
+* Jumbotron
+* Thumbnails
+* Alerts
+* Media Objects
+* List Groups
+* Panels
+* Wells
+####JavaScript
+* Tabs
+* Tooltip
+* Popover
+* Collapse (Accordion)
+* Modal
+
+== Installation ==
+1. Download and unzip this plugin
+1. Upload the "bootstrap-3-shortcodes" folder to your site's `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Create or edit a page or post and click the "B" button that appears above the editor to see the plugin's documentation!
+
+== Frequently Asked Questions ==
+
+= Does this plugin include Bootstrap 3? =
+
+No, we assume you are already working with a WordPress theme that includes the Bootstrap libraries.
